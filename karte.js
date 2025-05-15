8518c5b364da32c6f4ea7dbc50119c25d66248f0
Vierviertel <mail@anna-kalus.de>
2025-05-15 10:51:35 +0200

    Update karte.js

--- karte.js
+++ karte.js
@@ -3,50 +3,46 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidmllcnZpZXJ0ZWwiLCJhIjoiY21hbnN4c3V5MDJkeDJrc
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [10.4515, 51.1657],
+  center: [10.45, 51.16], // Deutschland-Mitte
   zoom: 5
 });
 
-chorDaten.features.forEach(feature => {
-  const { coordinates } = feature.geometry;
-  const {
-    name, beschreibung, leitung, saenger,
-    konzert, aufnahmestopp, bild, kontakt, link
-  } = feature.properties;
-
-  const el = document.createElement('div');
-  el.className = 'marker';
-  el.style.width = '32px';
-  el.style.height = '32px';
-  el.style.backgroundImage = 'url(https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png)';
-  el.style.backgroundSize = 'contain';
-  el.style.cursor = 'pointer';
-
-  const marker = new mapboxgl.Marker(el)
-    .setLngLat(coordinates)
-    .addTo(map);
+chorDaten.features.forEach((feature) => {
+  const coords = feature.geometry.coordinates;
+  const props = feature.properties;
 
   const popupHtml = `
     <div class="chor-popup">
-      <img class="chor-popup-img" src="${bild}" alt="${name}">
-      <div class="chor-popup-title">${name}</div>
-      <div class="chor-popup-desc">${beschreibung}</div>
+      <img class="chor-popup-img" src="${props.bild}" alt="${props.name}">
+      <div class="chor-popup-title">${props.name}</div>
+      <div class="chor-popup-desc">${props.beschreibung}</div>
       <hr class="chor-popup-line">
-      <div class="chor-popup-leitung">Leitung: ${leitung}</div>
+      <div class="chor-popup-leitung">Leitung: ${props.leitung}</div>
       <div class="chor-popup-stats">
-        <div><div class="label">Sänger:innen</div><div class="value">${saenger}</div></div>
-        <div><div class="label">Konzert</div><div class="value">${konzert}</div></div>
-        <div><div class="label">Aufnahme</div><div class="value">${aufnahmestopp ? 'Stopp' : 'Offen'}</div></div>
+        <div><div class="label">Sänger:innen</div><div class="value">${props.saenger}</div></div>
+        <div><div class="label">Konzert</div><div class="value">${props.konzert}</div></div>
+        <div><div class="label">Aufnahme</div><div class="value">${props.aufnahmestopp ? 'Stopp' : 'Offen'}</div></div>
       </div>
-      <a class="chor-popup-btn" href="${link}" target="_blank">Zur Website</a>
-      <div class="chor-popup-kontakt">${kontakt}</div>
+      <a class="chor-popup-btn" href="${props.link}" target="_blank">Zur Website</a>
+      <div class="chor-popup-kontakt">${props.kontakt}</div>
     </div>
   `;
 
   const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(popupHtml);
 
-  // Nur anzeigen, wenn geklickt wird
-  el.addEventListener('click', () => {
-    popup.setLngLat(coordinates).addTo(map);
+  const markerEl = document.createElement('div');
+  markerEl.className = 'marker';
+  markerEl.style.width = '32px';
+  markerEl.style.height = '32px';
+  markerEl.style.backgroundImage = 'url(https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png)';
+  markerEl.style.backgroundSize = 'contain';
+  markerEl.style.cursor = 'pointer';
+
+  const marker = new mapboxgl.Marker(markerEl)
+    .setLngLat(coords)
+    .addTo(map);
+
+  markerEl.addEventListener('click', () => {
+    popup.setLngLat(coords).addTo(map);
   });
 });
